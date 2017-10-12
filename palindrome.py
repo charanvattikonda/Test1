7587b19486548c97bcd1f43da15138c828e0a8e9
cvattik <you@example.com>
2017-10-12 21:38:59 +0000

    deleted script

--- palindrome.py
+++ palindrome.py
@@ -1,8 +1,2 @@
-#this is a script for palindrome
-while True:
-   a = int(input('enter the number'))
-   b = str(a)
-   if b[0] == b[-1]:
-      print 'its a palindrome'
-   elif b[0] != b[-1]:
-        print 'not a palindrome'
+# author cj
+     
